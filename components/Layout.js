8405694932e72a43fad11092e6c2f3acc9060ee6
mcmicko <mcmzrc@gmail.com>
2022-01-03 10:20:33 +0100

    Add Styles To Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Head from "next/head";
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import useStyles from "../utils/styles";
+import { mergeClasses } from "@material-ui/styles";
 
-function Layout() {
-  return <div>Layout</div>;
+function Layout({ children }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>Next Amazon</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Typography>amazona</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Next Amazona</Typography>
+      </footer>
+    </div>
+  );
 }
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Products</h1>
-      <ul>
-        <li>product1</li>
-        <li>product2</li>
-        <li>product3</li>
-      </ul>
-    </div>
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <ul>
+          <li>product1</li>
+          <li>product2</li>
+          <li>product3</li>
+        </ul>
+      </div>
+    </Layout>
   );
 }
