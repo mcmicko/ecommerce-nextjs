27a864ac031968917201ed4c8e5a7e1783eaf5eb
mcmicko <mcmzrc@gmail.com>
2022-01-04 17:03:54 +0100

    MatarialUI Theme

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,10 @@ import Head from "next/head";
 import {
   AppBar,
   Container,
+  createMuiTheme,
+  CssBaseline,
   Link,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from "@material-ui/core";
@@ -11,6 +14,32 @@ import useStyles from "../utils/styles";
 import NextLink from "next/link";
 
 function Layout({ title, description, children }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: "400",
+        margin: "1rem 0",
+      },
+      body1: {
+        fontWeight: "normal",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -18,26 +47,29 @@ function Layout({ title, description, children }) {
         <title>{title ? `${title} - Next Amazona` : `Next Amazon`}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/">
-            <Link>
-              <Typography className={classes.brand}>amazona</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <NextLink href="/cart">
-            <Link>Cart</Link>
-          </NextLink>
-          <NextLink href="/login">
-            <Link>login</Link>
-          </NextLink>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Next Amazona</Typography>
-      </footer>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/">
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <NextLink href="/cart">
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login">
+              <Link>login</Link>
+            </NextLink>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Next Amazona</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
