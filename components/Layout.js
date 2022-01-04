535bd6587813938b412657d8b2a9464203b3ea65
mcmicko <mcmzrc@gmail.com>
2022-01-04 17:40:11 +0100

    Create Dark Mode With Context And Set Cookie

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import {
   AppBar,
   Container,
-  createMuiTheme,
+  createTheme,
   CssBaseline,
   Link,
+  Switch,
   ThemeProvider,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 import useStyles from "../utils/styles";
 import NextLink from "next/link";
+import { Store } from "../utils/store";
+import Cookies from "js-cookie";
 
 function Layout({ title, description, children }) {
-  const theme = createMuiTheme({
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createTheme({
     typography: {
       h1: {
         fontSize: "1.6rem",
@@ -31,7 +36,7 @@ function Layout({ title, description, children }) {
       },
     },
     palette: {
-      type: "light",
+      type: darkMode ? "dark" : "light",
       primary: {
         main: "#f0c000",
       },
@@ -41,6 +46,11 @@ function Layout({ title, description, children }) {
     },
   });
   const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
   return (
     <div>
       <Head>
@@ -57,6 +67,10 @@ function Layout({ title, description, children }) {
               </Link>
             </NextLink>
             <div className={classes.grow}></div>
+            <Switch
+              checked={darkMode}
+              onChange={darkModeChangeHandler}
+            ></Switch>
             <NextLink href="/cart">
               <Link>Cart</Link>
             </NextLink>
